b3c1db44df95093123fe3fd2dd8db44233eaad68
Abdallah Aburomi <abdlaa1912@gmail.com>
2025-01-15 21:11:50 +0200

    car initial place and blocks

--- app/src/main/java/com/example/parking/ui/parking/ParkingSimulatorFragment.java
+++ app/src/main/java/com/example/parking/ui/parking/ParkingSimulatorFragment.java
@@ -41,24 +41,11 @@ public class ParkingSimulatorFragment extends Fragment {
         View view = inflater.inflate(R.layout.parking_simulator_layout, container, false);
         TextView cityView = view.findViewById(R.id.cityText);
         TextView parkView = view.findViewById(R.id.parkText);
-        TextView avblSlots = view.findViewById(R.id.avbl_slots);
-        TextView takenSlots = view.findViewById(R.id.taken_slots);
-        TextView unAvblSlots = view.findViewById(R.id.unavbl_slots);
-
-        GridLayout grid = view.findViewById(R.id.gridSlots);
 
         Button backButton = view.findViewById(R.id.back_park);
         backButton.setOnClickListener(v -> {
             NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_activity_main);
             navController.navigate(R.id.from_simulator_to_selection);
-        });
-
-        Button paymentButton = view.findViewById(R.id.paymentBtn);
-        paymentButton.setOnClickListener(v -> {
-            requireActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, new PaymentFragment()) // `R.id.fragment_container` should be the container for fragments in your activity layout
-                    .addToBackStack(null) // Optional: Add to back stack so user can navigate back
-                    .commit();
         });
 
        if (getArguments() == null){
@@ -90,72 +77,6 @@ public class ParkingSimulatorFragment extends Fragment {
                return null;
            }
 
-           int rows = park.getSlots().length;
-           int cols = park.getSlots()[0].length;
-
-           int avbl = 0;
-           int taken = 0;
-           int unAvbl = 0;
-
-           grid.setRowCount(rows);
-           grid.setColumnCount(cols);
-           // Loop through each slot and create an ImageView dynamically
-           for (int i = 0; i < rows; i++) {
-               for (int j = 0; j < cols; j++) {
-                   ImageView slotImageView = new ImageView(requireContext());
-
-                   if (park.getSlots()[i][j] == null) {
-                       avbl++;
-                       if (i == 0 && j == 0)
-                           slotImageView.setImageResource(R.drawable.ideal_slot);
-                        else
-                            slotImageView.setImageResource(R.drawable.avbl_slot);  // Green square for available
-
-                       slotImageView.setClickable(true);
-                       int finalI = i;
-                       int finalJ = j;
-                       String finalParkText = parkText;
-                       String finalCity = city;
-                       slotImageView.setOnClickListener(v ->
-                               showConfirmationDialog(finalCity, finalParkText, finalI, finalJ));
-                   } else if (!park.getSlots()[i][j].isDisabled()){
-                       taken++;
-                       slotImageView.setImageResource(R.drawable.taken_slot);  // Red square for taken
-                   }else{
-                       slotImageView.setImageResource(R.drawable.unavbl_slot);  // Red square for taken
-                        unAvbl++;
-                   }
-
-
-                   // Set layout parameters (width and height for each slot)
-                   GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-                   params.width = 200;
-                   params.height = 200;
-                   params.rowSpec = GridLayout.spec(i);
-                   params.columnSpec = GridLayout.spec(j);
-
-                   slotImageView.setLayoutParams(params);
-
-                   grid.addView(slotImageView);
-               }
-           }
-           StringBuilder sb = new StringBuilder();
-           sb.append(avbl);
-           sb.append(".");
-           if (avbl > 0)
-               avblSlots.setText(sb.toString());
-
-           sb = new StringBuilder();
-           sb.append(taken);
-           sb.append(".");
-           if (taken > 0)
-               takenSlots.setText(sb.toString());
-
-           sb = new StringBuilder();
-           sb.append(unAvbl);
-           sb.append(".");
-           if (unAvbl > 0)
-               unAvblSlots.setText(sb.toString());
        }
 
         return view;
